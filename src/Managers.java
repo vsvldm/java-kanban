7a31b6b4ff10c8022dae35fb8b3081d029b725cc
vsvldm <vsevolodmakarsky@yandex.ru>
2023-12-29 00:31:17 +0700

    feat: реализация менеджера

--- src/Managers.java
+++ src/Managers.java
@@ -2,7 +2,12 @@ public class Managers {
     public static TaskManager getDefaultTaskManager() {
         return new InMemoryTaskManager(new InMemoryHistoryManager());
     }
+
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
+    }
+
+    public static FileBackedTasksManager getFileBackedTasksManager() {
+        return new FileBackedTasksManager(new InMemoryHistoryManager());
     }
 }
